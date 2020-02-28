7f9a3f2185489eb7922b0b4f6863ab22813576dd
Hon <dennis.mejen@gmail.com>
2020-02-28 20:28:52 +0700

    Add answer to question 1295

--- 1295.js
+++ 1295.js
@@ -38,5 +38,11 @@
 // @return {number}
 
 var findNumbers = function(nums) {
-    
+  let count = 0;
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i].toString().length % 2 == 0) {
+      count++
+    }
+  }
+  return count;
 };
