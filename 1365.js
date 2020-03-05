6207ce2e16a080956e8863d063a76ebf3b490244
Hon <dennis.mejen@gmail.com>
2020-03-05 23:08:27 +0700

    Add answer to qs 1365

--- 1365.js
+++ 1365.js
@@ -43,5 +43,15 @@
 // @return {number[]}
 
 var smallerNumbersThanCurrent = function(nums) {
-    
+  let output = [];
+  for (i = 0; i < nums.length; i++) {
+    let counter = 0;
+    for (j = 0; j < nums.length; j++) {
+      if (nums[i] > nums[j]) {
+        counter++
+      }
+    }
+    output.push(counter);
+  }
+  return output;
 };
