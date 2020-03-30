f78fec32f0420cadb438f534f52f7cb130a4d401
Hon <dennis.mejen@gmail.com>
2020-03-31 00:44:27 +0700

    Add answer to qs 442

--- 2-Medium/442.js
+++ 2-Medium/442.js
@@ -25,5 +25,14 @@
 // @return {number[]}
 
 const findDuplicates = (nums) => {
+  let Output = [];
+  for (i = 0; i < nums.length; i++) {
+    for (j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        Output.push(nums[i]);
+        break;
+      }
+    }
+  }
   return Output;
 };
