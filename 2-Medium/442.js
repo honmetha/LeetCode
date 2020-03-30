9d092f4d96f70576a8924d5e9db2ac7c16fe2779
Hon <dennis.mejen@gmail.com>
2020-03-31 00:42:52 +0700

    Add question 442 to Medium

---
+++ 2-Medium/442.js
@@ -0,0 +1,29 @@
+// 442. Find All Duplicates in an Array
+
+// Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array),
+// some elements appear twice and others appear once.
+
+// Find all the elements that appear twice in this array.
+
+// Could you do it without extra space and in O(n) runtime?
+
+// Example:
+
+// Input:
+// [4,3,2,7,8,2,3,1]
+
+// Output:
+// [2,3]
+
+
+/***********************************************************************
+ *  Answer
+ **********************************************************************/
+
+
+// @param {number[]} nums
+// @return {number[]}
+
+const findDuplicates = (nums) => {
+  return Output;
+};
