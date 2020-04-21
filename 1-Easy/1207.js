8cabbfb5cd4a3b09d304ec44c0db52c3587f93e7
Hon <dennis.mejen@gmail.com>
2020-04-22 06:13:55 +0700

    Adding answers to qs 1207g

--- 1-Easy/1207.js
+++ 1-Easy/1207.js
@@ -48,3 +48,47 @@ const uniqueOccurrences = (arr) => {
   }
   return newArr2.length === new Set(newArr2).size;
 };
+
+
+
+// Just map each number to the number of its occurences, and put the values in a set. 
+// If two numbers have the same # of occurences, then the size would decrease since set's contain only unique values.
+const uniqueOccurrences = arr => {
+  const mapOccurrences = {};
+  for(let num of arr){
+      mapOccurrences[num] = (mapOccurrences[num] || 0) + 1;
+      console.log("mapOccurrences[num] =", mapOccurrences[num]);
+      console.log("mapOccurrences[num] || 0 =", mapOccurrences[num] || 0);
+  }
+  console.log(mapOccurrences);
+  return Object.keys(mapOccurrences).length === new Set(Object.values(mapOccurrences)).size
+};
+
+
+
+var uniqueOccurrences = function(arr) {
+  var map = {}, occurrences = {};
+  
+  // First step: 
+  // Loop and find each occurrences and store them against their values.
+  for (var i = 0; i < arr.length; ++ i) {
+      if (map[arr[i]] === undefined) {
+          map[arr[i]] = 0;
+      }
+      ++ map[arr[i]];
+  }
+  
+  // Second step: 
+  // Loop the stored map and store the occurrences in another map,
+  // While storing check if any value is repeated, if repeated return `false`
+  // Else `return true`
+  for (var num in map) {
+      if (map.hasOwnProperty(num)) {
+          if (occurrences[map[num]] !== undefined) {
+              return false;
+          }
+          occurrences[map[num]] = true;
+      }
+  }
+  return true;
+};
