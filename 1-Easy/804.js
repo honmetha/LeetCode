099a3ba1567e1ee23ad380d8089845147ed73b58
Hon <dennis.mejen@gmail.com>
2020-05-01 13:40:58 +0700

    Add answer to qs 804

--- 1-Easy/804.js
+++ 1-Easy/804.js
@@ -42,63 +42,10 @@
 // @param {string[]} words
 // @return {number}
 
-const uniqueMorseRepresentations = (words) => {
-  let MorseCode = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
-  let arr1 = [];
-  for (word of words) {
-    let arr2 = [];
-    for (letter of word) {
-      arr2.push(MorseCode[letter.charCodeAt(0) - 97]);
-    }
-    arr1.push(arr2.join(""));
-  }
-  return new Set(arr1).size;
-};
-
-
-
-// 48 ms, faster than 99.25%; 35.4 MB, less than 77.78%
-const uniqueMorseRepresentations = (words) => {
-  let arr = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
-  let set = new Set()
-  for(let i = 0; i < words.length; i++){
-      let str = ""
-      for(let j = 0; j < words[i].length; j++){
-          let code = words[i].charCodeAt(j) - 97;
-          code = arr[code];
-          str += code;
-      }
-      set.add(str);
-  }
-  return set.size;
-};
-
-
-
-// 52 ms, beats 94%
-// https://leetcode.com/problems/unique-morse-code-words/discuss/329303/JavaScript-One-Line-Solution-w-Explanation-(52-ms-beats-94)
-const uniqueMorseRepresentations = words => {  
+const uniqueMorseRepresentations = (words) => {  
   const alphabet = {
     a: '.-', b: '-...',   c: '-.-.', d: '-..', e: '.', f: '..-.', g: '--.', h: '....', i: '..',  j: '.---',  k: '-.-',  l: '.-..', m: '--',
     n: '-.',  o: '---', p: '.--.',  q: '--.-',  r: '.-.', s: '...', t: '-', u: '..-', v: '...-', w: '.--', x: '-..-',  y: '-.--', z: '--..' 
-  }
+  };
   return new Set(words.map(word => word.split('').map(letter => alphabet[letter]).join(''))).size
 }
-
-
-
-// Javascript faster than 93.53%
-var uniqueMorseRepresentations = function(words) {
-  const MORSE_CODE = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
-  
-  const uniques = new Set()
-  
-  words.forEach(word => {
-      let transform = [];
-      for (let letter of word) {
-          transform.push(MORSE_CODE[letter.charCodeAt(0) - 97])
-      }
-      uniques.add(transform.join(''))
-  })
-  return uniques.size;
-};
