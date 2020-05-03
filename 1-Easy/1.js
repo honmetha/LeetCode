af9119afa9401b0b51d9863b2c163e5f6d9727ad
Hon <dennis.mejen@gmail.com>
2020-05-03 15:29:40 +0700

    Add answer to qs 1

--- 1-Easy/1.js
+++ 1-Easy/1.js
@@ -22,10 +22,15 @@
 // @return {number[]}
 
 const twoSum = (nums, target) => {
+  const previousValues = {}
   for (i = 0; i < nums.length; i++) {
-    let x = target - nums[i];
-    for (j = i + 1; j < nums.length; j++ ) {
-      if (x - nums[j] === 0) return [i, j];
+    const currentNumber = nums[i];
+    const neededValue = target - currentNumber;
+    const index2 = previousValues[neededValue];
+    if (index2 != null) {
+      return [index2, i]
+    } else {
+      previousValues[currentNumber] = i;
     }
   }
 };
