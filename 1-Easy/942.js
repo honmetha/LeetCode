0fc5ef0b6b19990e1c41de5e77219b0293c1562e
Hon <dennis.mejen@gmail.com>
2020-05-21 07:34:38 +0700

    Add answer to qs 942

--- 1-Easy/942.js
+++ 1-Easy/942.js
@@ -39,67 +39,16 @@
 // @return {number[]}
 
 const diStringMatch = S => {
-  let output = [];
-  let N = Array.from(Array(S.length + 1).keys());
-
-  for (item of S) {
-    if (item === "I") {
-      output.push(N[0]);
-      N.shift();
-    } else {
-      output.push(N[N.length - 1]);
-      N.pop();
-    }
-  }
-  output.push(N[0]);
-
-  return output;
-};
-
-
-
-const diStringMatch = S => {
-  let output = [];
-  let N = Array.from(Array(S.length + 1).keys());
-  for (item of S) item === "I" ? output.push(N.shift()) : output.push(N.pop());
-  output.push(N[0]);
-  return output;
-};
-
-
-
-const diStringMatch = S => {
   let output = [], x = 0, y = S.length;
   for (item of S) {
     if (item === "I") {
-      output = [...output, x];
+      output.push(x);
       x++;
     } else {
-      output = [...output, y];
+      output.push(y);
       y--;
     }
   }
-  return [...output, x];
-};
-
-
-
-// Javascript solution two pointer(98.31% time. 100% space)
-// https://leetcode.com/problems/di-string-match/discuss/557160/Javascript-solution-two-pointer(98.31-time.-100-space)
-// Runtime: 72 ms, faster than 98.31% of JavaScript online submissions for DI String Match.
-// Memory Usage: 37.1 MB, less than 100.00% of JavaScript online submissions for DI String Match.
-var diStringMatch = function(S) {
-  var permutation = [];
-  var min = 0, max = S.length
-  for(var i = 0; i < S.length; i++) {
-    if(S[i] === 'I') {
-      permutation.push(min)
-      min++;
-    } else {
-      permutation.push(max)
-      max--;
-    }
-  }
-  permutation.push(min);
-  return permutation;
+  output.push(x);
+  return output;
 };
