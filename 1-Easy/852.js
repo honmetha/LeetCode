4768c3550b444ec1be726f63db53a05cf8a097c6
Hon <dennis.mejen@gmail.com>
2020-05-29 07:34:02 +0700

    Add alternative answers

--- 1-Easy/852.js
+++ 1-Easy/852.js
@@ -39,11 +39,38 @@ const peakIndexInMountainArray = A => A.indexOf(Math.max(...A));
 
 const peakIndexInMountainArray = A => {
   let idx = 0;
-  while (A[idx] < A[idx + 1]) {
-    idx++;
-  }
+  while (A[idx] < A[idx + 1]) idx++;
   return idx;
 };
 
 // test case
 // [24,69,100,99,79,78,67,36,26,19]
+
+
+
+// JavaScript 48ms
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/discuss/610561/JavaScript-48ms
+var peakIndexInMountainArray = function(A) {
+  let left = 0;
+  let right = A.length-1;
+
+  while(left<=right){
+      let mid = Math.floor(left + ((right-left)>>2))
+      if(A[mid] >A[mid-1] && A[mid]>A[mid+1] ){
+          return mid;
+      }else if(A[mid+1] > A[mid]){
+          left = mid+1;
+      }else {
+          right = mid-1;
+      }
+  }
+  return 0;
+};
+
+
+// JavaScript One-Line Solution with Reduce
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/discuss/513235/JavaScript-One-Line-Solution-with-Reduce
+// This should be a better than the "index of spread max" one-line solution.
+// Runtime: 52 ms, faster than 88.71% of JavaScript online submissions
+// Memory Usage: 35 MB, less than 70.00% of JavaScript online submissions
+const peakIndexInMountainArray = A => A.reduce((acc, curr, i) => (A[acc] < curr ? i : acc), 0);
