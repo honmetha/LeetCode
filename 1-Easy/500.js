47530685217c90ba74f137a0d1b6220d8ad067db
Hon <dennis.mejen@gmail.com>
2020-06-24 10:47:32 +0700

    Add problem 500 and update README.md

---
+++ 1-Easy/500.js
@@ -0,0 +1,28 @@
+// 500. Keyboard Row
+
+// Given a List of words, return the words that can be typed using letters of 
+// alphabet on only one row's of American keyboard like the image below.
+ 
+// Example:
+
+// Input: ["Hello", "Alaska", "Dad", "Peace"]
+// Output: ["Alaska", "Dad"]
+
+
+// Note:
+
+// You may use one character in the keyboard more than once.
+// You may assume the input string will only contain letters of alphabet.
+
+
+/***********************************************************************
+ *  Answer
+ **********************************************************************/
+
+
+// @param {string[]} words
+// @return {string[]}
+
+const findWords = words => {
+  return output;
+};
