217d69c26453395dd2bff994aa270721bce46883
Hon <dennis.mejen@gmail.com>
2020-08-16 00:23:09 +0700

    [Updated] Update solutions and runtime of problem 1046

--- 1-Easy/1046.js
+++ 1-Easy/1046.js
@@ -34,6 +34,7 @@
 // @param {number[]} stones
 // @return {number}
 
+// Original
 const lastStoneWeight = stones => {
   while (stones.length > 1) {
     stones.sort((a, b) => b - a);
@@ -48,21 +49,5 @@ const lastStoneWeight = stones => {
   return stones[0] ? stones[0] : 0;
 };
 
-
-// JavaScript Recursive One-Liner
-// Runtime: 64 ms, faster than 52.83% of JavaScript online submissions
-// Memory Usage: 35.2 MB, less than 100.00% of JavaScript online submissions
-// https://leetcode.com/problems/last-stone-weight/discuss/498199/JavaScript-Recursive-One-Liner
-const lastStoneWeight = s => 1 === s.length ? s[0] : lastStoneWeight(s.sort((a, b) => a - b).concat(s.pop() - s.pop()));
-
-
-// JavaScript recursive solution 64ms
-// https://leetcode.com/problems/last-stone-weight/discuss/294934/JavaScript-recursive-solution-64ms
-const lastStoneWeight = stones => {
-  const len = stones.length;
-  if(len === 1) return stones;
-  stones.sort((a,b) => a-b);
-  const last = stones[len - 1];
-  const secToLast = stones[len-2];
-  return lastStoneWeight(stones.slice(0,stones.length-2).concat(last-secToLast));
-};
+// Recursive One-Liner
+const lastStoneWeight = s => s.length === 1 ? s[0] : lastStoneWeight(s.sort((a, b) => a - b).concat(s.pop() - s.pop()));
