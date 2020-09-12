17a96f56a2b8949d98a76eb01694e48b13554dee
Hon <dennis.mejen@gmail.com>
2020-09-12 09:00:17 +0700

    [Added] Add solution, runtime and memory to problem 158

--- 1-Easy/168.js
+++ 1-Easy/168.js
@@ -34,5 +34,36 @@
 // @return {string}
 
 const convertToTitle = n => {
+  const valueToLetter = {1:"A", 2:"B", 3:"C", 4:"D", 5:"E", 6:"F", 7:"G", 8:"H", 9:"I", 10:"J", 11:"K", 12:"L", 13:"M", 14:"N", 15:"O", 16:"P", 17:"Q", 18:"R", 19:"S", 20:"T", 21:"U", 22:"V", 23:"W", 24:"X", 25:"Y", 0:"Z"};
+  let result = "";
+
+  while (n > 0) {
+    result = valueToLetter[n % 26] + result;
+    if (n / 26 > 1) n = (n - (n % 26 || 26)) / 26;
+    else n -= (n % 26 || 26);
+  }
+
   return result;
 };
+
+// Test cases
+// 0
+// 5
+// 26
+// 28
+// 52
+// 100
+// 267
+// 701
+// 1234
+// 98768
+// 5678788
+// 1000000000
+// 1198767876
+// 1231111111
+
+const testFunction = () => {
+  for (i = 500000; i < 500100; i++) {
+    convertToTitle(i);
+  }
+}
