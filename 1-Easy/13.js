90fb1f647a2028d1d0ddf85469e16b37397e1565
Hon <dennis.mejen@gmail.com>
2020-09-23 21:44:53 +0700

    [Added] Add cleaner solution to problem 13

--- 1-Easy/13.js
+++ 1-Easy/13.js
@@ -57,8 +57,9 @@
 // @param {string} s
 // @return {number}
 
+// Original
 const romanToInt = s => {
-  const symbol = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000}
+  const symbol = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000};
   let result = symbol[s[0]] || 0;
 
   for (let i = 1; i < s.length; i++) {
@@ -67,4 +68,25 @@ const romanToInt = s => {
   }
 
   return result;
+};
+
+// Cleaner
+const romanToInt = s => {
+  symbols = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+  };
+
+  let value = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    symbols[s[i]] < symbols[s[i + 1]] ? value -= symbols[s[i]]: value += symbols[s[i]];
+  }
+
+  return value;
 };
