e12f78e51a167220d5f0467185c31c65989b5df6
Hon <dennis.mejen@gmail.com>
2020-10-14 23:20:27 +0700

    [Added] Add alternative solutions to problem 58

--- 1-Easy/58.js
+++ 1-Easy/58.js
@@ -21,7 +21,23 @@
 // @param {string} s
 // @return {number}
 
+// Original
 const lengthOfLastWord = s => /^[a-z]/i.test(s.replace(/ /g,"")) ? s.split(" ").filter(item => /^[a-z]/i.test(item)).pop().length : 0;
+
+// Simple One-Liner
+const lengthOfLastWord = s => s.trim().split(" ").pop().length;
+
+
+// JavaScript Solution O(n) Left-to-Right
+// https://leetcode.com/problems/length-of-last-word/discuss/509411/JavaScript-Solution-O(n)-Left-to-Right
+const lengthOfLastWord = s => {
+  let length = 0,
+  max = 0;
+  for (let i = 0; i < s.length; i++)
+    if (s[i] !== " ") length++, (max = length);
+    else length = 0;
+  return max;
+};
 
 // Test cases
 // ""
